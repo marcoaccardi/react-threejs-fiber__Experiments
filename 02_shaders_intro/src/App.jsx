5804b870a060897141a9749e314cff9c3779697c
sonotype <contact.marco.accardi@gmail.com>
2022-09-10 18:08:41 +0200

    apply texture to mesh

--- 02_shaders_intro/src/App.jsx
+++ 02_shaders_intro/src/App.jsx
@@ -1,13 +1,17 @@
 import * as THREE from "three";
 import { useRef, Suspense } from "react";
-import { Canvas, extend, useFrame } from "@react-three/fiber";
+import { Canvas, extend, useFrame, useLoader } from "@react-three/fiber";
 import { shaderMaterial } from "@react-three/drei";
 import glsl from "babel-plugin-glsl/macro";
 import "./App.scss";
 
 const WaveShaderMaterial = shaderMaterial(
   //Uniform
-  { uTime: 0, uColor: new THREE.Color(0.0, 0.0, 0.0, 0.0) },
+  {
+    uTime: 0,
+    uColor: new THREE.Color(0.0, 0.0, 0.0, 0.0),
+    uTexture: new THREE.Texture(),
+  },
   //Vertex Shader
   glsl`
   precision mediump float;
@@ -15,6 +19,7 @@ const WaveShaderMaterial = shaderMaterial(
   varying vec2 vUv;
   uniform float uTime;
   #pragma glslify: snoise3 = require(glsl-noise/simplex/3d);
+
   void main(){
     vUv = uv;
 
@@ -31,13 +36,14 @@ const WaveShaderMaterial = shaderMaterial(
   precision mediump float;
 
   uniform vec3 uColor;
-
   uniform float uTime;
+  uniform sampler2D uTexture;
 
   varying vec2 vUv;
 
   void main(){
-    gl_FragColor = vec4(sin(vUv.x + uTime) * uColor, 0.8);
+    vec3 texture = texture2D(uTexture, vUv).rgb;
+    gl_FragColor = vec4(texture, 0.8);
   }
   `
 );
@@ -48,10 +54,15 @@ extend({ WaveShaderMaterial });
 const Wave = () => {
   const ref = useRef();
   useFrame(({ clock }) => (ref.current.uTime = clock.getElapsedTime()));
+
+  const [image] = useLoader(THREE.TextureLoader, [
+    "https://images.unsplash.com/photo-1435820394963-a15297f976fd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit",
+  ]);
+
   return (
     <mesh>
       <planeGeometry args={[0.4, 0.6, 16, 16]} />
-      <waveShaderMaterial uColor={"hotpink"} ref={ref} wireframe />
+      <waveShaderMaterial uColor={"hotpink"} ref={ref} uTexture={image} />
     </mesh>
   );
 };
