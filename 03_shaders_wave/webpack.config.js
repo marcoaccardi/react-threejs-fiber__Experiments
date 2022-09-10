71fa2f49337a8d183f2eaed689565f43ed82186e
sonotype <contact.marco.accardi@gmail.com>
2022-09-11 00:28:43 +0200

    setting up shader files

---
+++ 03_shaders_wave/webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        // exclude: /node_modules/,
+        use: ["raw-loader", "glslify-loader"],
+      },
+    ],
+  },
+};
