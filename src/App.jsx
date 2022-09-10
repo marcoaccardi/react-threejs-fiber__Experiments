b1a765bf926b8c85bcc480fb4a526a491abfc374
sonotype <contact.marco.accardi@gmail.com>
2022-09-10 14:58:41 +0200

    control mesh position for  geometry istances

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useRef } from "react";
 import "./App.scss";
 import { Canvas, useFrame } from "@react-three/fiber";
 
-const Box = () => {
+const SpinningMesh = ({ position }) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
   return (
-    <mesh ref={mesh}>
+    <mesh position={position} ref={mesh}>
       <boxGeometry attach='geometry' args={[1, 1, 1]} />
       <meshStandardMaterial attach='material' color='pink' />
     </mesh>
@@ -15,9 +15,11 @@ const Box = () => {
 const App = () => {
   return (
     // <div className='canvas-container'>
-    <Canvas>
+    <Canvas colormanagement='true' camera={{ position: [-5, 2, 10], fov: 60 }}>
       <ambientLight intensity={0.7} />
-      <Box />
+      <SpinningMesh position={[0, 1, 0]} />
+      <SpinningMesh position={[-2, 1, -5]} />
+      <SpinningMesh position={[5, 1, -2]} />
     </Canvas>
     // </div>
   );
