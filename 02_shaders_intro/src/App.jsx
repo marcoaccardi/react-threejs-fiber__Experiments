30a96d247e65efc45f16417d3b6cc56083611626
sonotype <contact.marco.accardi@gmail.com>
2022-09-10 17:58:50 +0200

    setup shader with simplex-noise

--- 02_shaders_intro/src/App.jsx
+++ 02_shaders_intro/src/App.jsx
@@ -1,23 +1,43 @@
 import * as THREE from "three";
-import { Canvas, extend } from "@react-three/fiber";
+import { useRef, Suspense } from "react";
+import { Canvas, extend, useFrame } from "@react-three/fiber";
 import { shaderMaterial } from "@react-three/drei";
 import glsl from "babel-plugin-glsl/macro";
 import "./App.scss";
 
 const WaveShaderMaterial = shaderMaterial(
   //Uniform
-  { uColor: new THREE.Color(0.0, 0.0, 0.0, 0.0) },
+  { uTime: 0, uColor: new THREE.Color(0.0, 0.0, 0.0, 0.0) },
   //Vertex Shader
   glsl`
+  precision mediump float;
+
+  varying vec2 vUv;
+  uniform float uTime;
+  #pragma glslify: snoise3 = require(glsl-noise/simplex/3d);
   void main(){
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    vUv = uv;
+
+    vec3 pos = position;
+    float noiseFreq = 2.5;
+    float noiseAmp = 0.65;
+    vec3 noisePos = vec3(pos.x * noiseFreq + uTime, pos.y, pos.z);
+    pos.z += snoise3(noisePos) * noiseAmp;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
   }
   `,
   // Fragment Shader
   glsl`
+  precision mediump float;
+
   uniform vec3 uColor;
+
+  uniform float uTime;
+
+  varying vec2 vUv;
+
   void main(){
-    gl_FragColor = vec4(uColor, 0.8);
+    gl_FragColor = vec4(sin(vUv.x + uTime) * uColor, 0.8);
   }
   `
 );
@@ -25,14 +45,23 @@ const WaveShaderMaterial = shaderMaterial(
 // shaders as JSX component
 extend({ WaveShaderMaterial });
 
+const Wave = () => {
+  const ref = useRef();
+  useFrame(({ clock }) => (ref.current.uTime = clock.getElapsedTime()));
+  return (
+    <mesh>
+      <planeGeometry args={[0.4, 0.6, 16, 16]} />
+      <waveShaderMaterial uColor={"hotpink"} ref={ref} wireframe />
+    </mesh>
+  );
+};
+
 const Scene = () => {
   return (
-    <Canvas>
-      <pointLight position={[10, 10, 10]} />
-      <mesh>
-        <planeGeometry args={[3, 5]} />
-        <waveShaderMaterial uColor={"hotpink"} />
-      </mesh>
+    <Canvas camera={{ fov: 10, position: [0, 0, 5] }}>
+      <Suspense fallback={null}>
+        <Wave />
+      </Suspense>
     </Canvas>
   );
 };
