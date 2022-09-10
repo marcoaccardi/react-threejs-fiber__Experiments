c86d9a2a60683541fca6a0bda0472d387cbef32a
sonotype <contact.marco.accardi@gmail.com>
2022-09-10 14:52:30 +0200

    create cube with mesh, lighting, useFrame for the animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import { useRef } from "react";
 import "./App.scss";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+const Box = () => {
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+  return (
+    <mesh ref={mesh}>
+      <boxGeometry attach='geometry' args={[1, 1, 1]} />
+      <meshStandardMaterial attach='material' color='pink' />
+    </mesh>
+  );
+};
 const App = () => {
   return (
-    <div className='canvas-container'>
-      <Canvas>
-        <mesh>
-          <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
-        </mesh>
-      </Canvas>
-    </div>
+    // <div className='canvas-container'>
+    <Canvas>
+      <ambientLight intensity={0.7} />
+      <Box />
+    </Canvas>
+    // </div>
   );
 };
 
