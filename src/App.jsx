d5b67543803708738c45ad5290d7f3d8e48aaf09
sonotype <contact.marco.accardi@gmail.com>
2022-09-10 15:36:15 +0200

    add shadow casting

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const SpinningMesh = ({ position, args, color }) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
   return (
-    <mesh position={position} ref={mesh}>
+    <mesh castShadow position={position} ref={mesh}>
       <boxGeometry attach='geometry' args={args} />
       <meshStandardMaterial attach='material' color={color} />
     </mesh>
@@ -16,9 +16,14 @@ const SpinningMesh = ({ position, args, color }) => {
 const App = () => {
   return (
     // <div className='canvas-container'>
-    <Canvas colormanagement='true' camera={{ position: [-5, 2, 10], fov: 60 }}>
+    <Canvas
+      shadows
+      colormanagement='true'
+      camera={{ position: [-5, 2, 10], fov: 60 }}
+    >
       <ambientLight intensity={0.7} />
       <directionalLight
+        castShadow
         position={[0, 10, 0]}
         intensity={1.5}
         shadowMapWidth={1024}
@@ -32,9 +37,13 @@ const App = () => {
       <pointLight position={[-10, 0, -20]} intensity={1.5} />
       <pointLight position={[0, -10, 0]} intensity={0.5} />
       <group>
-        <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -5, 0]}>
+        <mesh
+          receiveShadow
+          rotation={[-Math.PI / 2, 0, 0]}
+          position={[0, -3, 0]}
+        >
           <planeGeometry attach='geometry' args={[100, 100]} />
-          <meshStandardMaterial attach='material' color='yellow' />
+          <shadowMaterial attach='material' />
         </mesh>
       </group>
       <SpinningMesh position={[0, 1, 0]} args={[3, 2, 1]} color='lightblue' />
